8cd8c2661fedba6ddb0c56200bc4590a05a93b5e
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2023-12-19 20:50:41 +0900

    Make fragment prefix and suffix constants

--- lib/epub/cfi/parser.rb
+++ lib/epub/cfi/parser.rb
@@ -4,6 +4,9 @@ class EPUB::CFI::Parser
   include Comparable
 
   UNICODE_CHARACTER_EXCLUDING_SPECIAL_CHARS_AND_SPACE_AND_DOT_AND_COLON_AND_TILDE_AND_ATMARK_AND_SOLIDUS_AND_EXCLAMATION_MARK_PATTERN = /\u0009|\u000A|\u000D|[\u0022-\u0027]|[\u002A-\u002B]|\u002D|[\u0030-\u0039]|\u003C|[\u003E-\u0040]|[\u0041-\u005A]|\u005C|[\u005F-\u007D]|[\u007F-\uD7FF]|[\uE000-\uFFFD]|[\u10000-\u10FFFF]/ # excluding special chars and space(\u0020) and dot(\u002E) and colon(\u003A) and tilde(\u007E) and atmark(\u0040) and solidus(\u002F) and exclamation mark(\u0021)
+  FRAGMENT_PREFIX = "epubcfi("
+  FRAGMENT_PREFIX_LENGTH = FRAGMENT_PREFIX.length
+  FRAGMENT_SUFFIX_LENGTH = ")".length
 
   class << self
     def parse(string, debug: $DEBUG)
@@ -17,8 +20,8 @@ class EPUB::CFI::Parser
   end
 
   def parse(string)
-    if string.start_with? 'epubcfi('
-      string = string['epubcfi('.length .. -2]
+    if string.start_with? FRAGMENT_PREFIX
+      string = string[FRAGMENT_PREFIX_LENGTH .. -(FRAGMENT_SUFFIX_LENGTH + 1)]
       # @type var string: String
     end
     @scanner = StringScanner.new(string, true)
